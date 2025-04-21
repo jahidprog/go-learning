d3f2ed0957cfb5059d4fe48089cbab791845e8ea
Jahidul Islam <jahid.prog@gmail.com>
2025-04-21 22:54:55 +0600

    update class25 cmnt

--- Function/class025.go
+++ Function/class025.go
@@ -3,43 +3,47 @@ package main
 
 import "fmt"
 
+// Constant value - compile time এ determine হয় এবং পরিবর্তন করা যায় না
 const a = 10
 
+// Global variable - package level এ define করা হয়েছে, পুরো ফাইলে access করা যায়
 var p = 100
 
+// call(): একটি ফাংশন, যেখানে একটি function expression (anonymous function) define ও call করা হয়েছে
 func call() {
 	add := func(x, y int) {
 		z := x + y
 		fmt.Println(z)
 	}
 
-	add(5, 6)
-	add(p, a)
-}
-func main() {
-	call()
-	fmt.Println(a)
+	add(5, 6) // 5 + 6 = 11
+	add(p, a) // 100 + 10 = 110
 }
 
+func main() {
+	call()         // call() ফাংশন কল
+	fmt.Println(a) // constant 'a' এর মান প্রিন্ট
+}
+
+// init(): Go প্রোগ্রামে main এর আগে init() ফাংশন অটো কল হয় (একটি special function)
 func init() {
 	fmt.Println("Hello!")
 }
 
 /*
-	there are two phases in go program
-	1. compilation phases
-		go have it's own compiler-> after compiling this file with will generate a binary file
-		cmd: go build main.go --> will create binay file
-		for runing this binary file cmd: ./main
+	Go প্রোগ্রামে দুইটি ধাপ থাকে:
 
-	cmd:
-		go build main.go => compile it => main
-		./main
-		go run main.go --> compile it(go build main.go) --> ./main
+	1. Compilation Phase (কম্পাইলেশন ধাপ)
+		- Go এর নিজস্ব কম্পাইলার রয়েছে (go compiler)
+		- source file (.go) compile করে একটি binary file তৈরি করে
+		- কমান্ড:
+			- go build main.go      → এটি compile করে একটি binary তৈরি করবে (main নামে)
+			- ./main                → কম্পাইল হওয়া binary ফাইল রান করাবে
+			- go run main.go        → এটি ভেতরে ভিতরে আগে go build করে, তারপর ./main রান করে
 
-	2. execution phases
-		execute hoy line by line
-
+	2. Execution Phase (এক্সিকিউশন ধাপ)
+		- কম্পাইল হওয়ার পর প্রোগ্রাম লাইন বাই লাইন execute হয়
+		- প্রথমে init() → তারপর main() execute হয়
 */
 
 /*
---
+++ Function/class026.go
@@ -0,0 +1,11 @@
+// Clousure
+package main
+
+import "fmt"
+
+func main() {
+
+}
+func init() {
+	fmt.Println("Init")
+}
